04172061013960b222c01c29e22b0e6a52e49941
Joel Gingrich <jg2446@cs.ship.edu>
2019-11-14 21:48:55 -0500

    added to draw square

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -28,7 +28,7 @@ public class GUI extends JFrame {
   JLabel[][] labelArray;
   JLabel stats;
   JPanel board = new JPanel(new GridBagLayout());
-  
+
   public GUI(Environment env) {
     this.env = env;
     setLayout(new GridBagLayout());
@@ -36,7 +36,7 @@ public class GUI extends JFrame {
     setMouseListener();
     drawStats(env.getCell(0, 0));
     drawLegend();
-    
+
     pack();
     setResizable(false);
     setVisible(true);
@@ -59,6 +59,7 @@ public class GUI extends JFrame {
     x.gridy = 0;
     add(board, x);
   }
+
   /*
    * Redraws the board.
    */
@@ -68,20 +69,24 @@ public class GUI extends JFrame {
         labelArray[r][c].setIcon(createSquare(env.getCell(r, c)));
       }
     }
+    pack();
+
   }
-  
+
   public void redrawStats() {
     stats.setText(env.getSelectedCell().getStats());
   }
+
   private void drawStats(Cell c) {
     stats = new JLabel(c.getStats());
     stats.setLocation(0, 0);
     stats.setOpaque(true);
-    stats.setBackground(new Color(255,255,255));
+    stats.setBackground(new Color(255, 255, 255));
     x.anchor = x.FIRST_LINE_START;
     x.gridx = 1;
     x.gridy = 0;
     add(stats, x);
+    pack();
   }
 
   /*
@@ -97,34 +102,41 @@ public class GUI extends JFrame {
     x.gridx = 0;
     x.gridy = 1;
     add(legend, x);
+
   }
 
   private ImageIcon createSquare(Cell c) {
     BufferedImage i = new BufferedImage(50, 50, BufferedImage.TYPE_3BYTE_BGR);
     Graphics g = i.getGraphics();
-    
-    //selected cell
-    if(c == env.getSelectedCell()) {
+
+    // selected cell
+    if (c == env.getSelectedCell()) {
       g.setColor(new Color(150, 200, 150));
       g.fillRect(0, 0, 50, 50);
       g.setColor(Color.WHITE);
       g.drawRect(49, 0, 1, 50);
       g.drawRect(0, 49, 50, 1);
-    }
-    else {
-      g.setColor(new Color(100, 100, 100));
+    } else {
+      g.setColor(new Color(204, 204, 204));
       g.fillRect(0, 0, 50, 50);
       g.setColor(Color.WHITE);
       g.drawRect(49, 0, 1, 50);
       g.drawRect(0, 49, 50, 1);
     }
-    
-    if(c.hasLifeForm()) {
-      if(c.getLifeForm() instanceof Alien) {
+
+    // life forms
+    if (c.hasLifeForm()) {
+
+      if (c.getLifeForm().hasWeapon()) {
+        g.setColor(new Color(153, 153, 153));
+        g.fillRect(35, 5, 10, 20);
+      }
+
+      // draw life forms
+      if (c.getLifeForm() instanceof Alien) {
         g.setColor(new Color(255, 0, 0));
         g.fillRect(10, 10, 30, 30);
-      }
-      else if(c.getLifeForm() instanceof Human) {
+      } else if (c.getLifeForm() instanceof Human) {
         g.setColor(new Color(0, 255, 255));
         g.fillOval(10, 10, 30, 30);
       }
@@ -137,11 +149,11 @@ public class GUI extends JFrame {
     Graphics g = i.getGraphics();
     return new ImageIcon(getClass().getResource("CommandButtons/legend.png"));
   }
-  
+
   private void onMouseClicked(MouseEvent e) {
     for (int r = 0; r < env.getNumRows(); r++) {
       for (int c = 0; c < env.getNumCols(); c++) {
-        if(e.getSource() == labelArray[r][c]) {
+        if (e.getSource() == labelArray[r][c]) {
           env.selectCell(r, c);
           redrawBoard();
           redrawStats();
@@ -149,6 +161,7 @@ public class GUI extends JFrame {
       }
     }
   }
+
   public void setMouseListener() {
     for (int r = 0; r < env.getNumRows(); r++) {
       for (int c = 0; c < env.getNumCols(); c++) {
